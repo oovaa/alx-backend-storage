d5d6836440e6bf7b88e4763c93982642ad02cc73
oovaa <omarvx211@gmail.com>
2024-06-13 10:28:00 +0200

    started sql advanced task

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,15 @@
+-- task 0
+/*
+ * Creates a table named 'users' if it does not already exist.
+ * The table has three columns:
+ * - id: an auto-incrementing integer column that serves as the primary key.
+ * - email: a non-null varchar column with a maximum length of 255 characters, which must be unique.
+ * - name: a varchar column with a maximum length of 255 characters.
+ */
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
